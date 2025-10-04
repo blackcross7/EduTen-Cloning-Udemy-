b9d590490a25c11e42f42b6ff4a96893b9ef9121
rahul-kumar620 <rahulkumarjsrbhu@gmail.com>
2025-10-04 12:20:51 +0530

    work on it section

---
+++ client/src/components/ITSoftware/data/mockData.js
@@ -0,0 +1,122 @@
+export const popularHoverDetails = [
+  {
+    title: "Ultimate AWS Certified Solutions Architect Associate 2025)",
+    premium: true,
+    bestseller: true,
+    updated: "August 2025",
+    duration: "32 total hours",
+    level: "All Levels",
+    subtitles: true,
+    description:
+      "dfghjkl;fghujkl;dfghjkl;dfghjklxcvbnm,.sdfghjkl;dfghjkl;zxcvbnm,.sdfghjkl;xcvbnm,./dfghjkl;",
+    points: [
+      "Apply theoretical models such as Beck’s Cognitive Therapy and REBT",
+      "Develop self-discipline for mindset, thinking, and emotions",
+    ],
+  },
+  {
+    title: "Meditation Masterclass: meditation teacher certification",
+    premium: false,
+    bestseller: true,
+    updated: "March 2025",
+    duration: "5.5 total hours",
+    level: "All Levels",
+    subtitles: true,
+    description:
+      "Accredited meditation certification course. Learn & teach a wide range of meditation techniques from around the world!",
+    points: ["Meditation", "Mindfulness", "Breathwork"],
+  },
+  {
+    title: "Herbalism :: Introduction & Medicine Making Certificate",
+    premium: true,
+    bestseller: true,
+    updated: "April 2025",
+    duration: "4 total hours",
+    level: "All Levels",
+    subtitles: true,
+    description:
+      "Take charge of your health with herbal medicine. Using herbs and natural holistic medicine is easy, effective and safe.",
+    points: [
+      "Learn life-changing herbalism skills in one place",
+      "Details of 20+ important medicinal plants",
+    ],
+  },
+  {
+    title: "The Complete Fitness & Health Masterclass - 21 Courses in 1",
+    premium: true,
+    bestseller: true,
+    updated: "October 2020",
+    duration: "23 total hours",
+    level: "All Levels",
+    subtitles: true,
+    description:
+      "The Most Comprehensive Masterclass to Improve Your Mind, Body and Health - Start Growing Today!",
+    points: [
+      "Discover what Reverse Aging is & how it works",
+      "Learn the science & secrets behind Reverse Aging",
+    ],
+  },
+  {
+    title: "Internationally Accredited Certificate in Nutrition",
+    premium: true,
+    bestseller: true,
+    updated: "July 2025",
+    duration: "5 total hours",
+    level: "All Levels",
+    subtitles: true,
+    description:
+      "Become a Nutrition Coach: Learn the Fundamentals of Nutrition Science",
+    points: [
+      "Understand Macronutrients and their role in the body",
+      "Understand Micronutrients, Fiber, Probiotics & Water",
+      "Learn Energy Balance and methods to determine BMR, TDEE",
+    ],
+  },
+  {
+    title: "Art Therapy Life Coach Certification (Accredited)",
+    premium: true,
+    bestseller: true,
+    updated: "August 2025",
+    duration: "3.5 total hours",
+    level: "All Levels",
+    subtitles: true,
+    description:
+      "Help Clients Heal, Reduce Stress + Manifest Dreams with Mandalas, Affirmations, Vision Boards + More Art Therapy Tools",
+    points: [
+      "Understand the psychology of therapeutic art",
+      "Help clients manage emotions positively",
+      "Use art therapy to reduce stress and heal emotional pain",
+    ],
+  },
+  {
+    title: "Meditation for Beginners: Boost Your Productivity in 5 Days",
+    premium: false,
+    bestseller: false,
+    updated: "July 2022",
+    duration: "1.5 total hours",
+    level: "Beginner",
+    subtitles: true,
+    description:
+      "Meditation will help you increase focus, boost productivity, achieve your goals.",
+    points: [
+      "Increase focus through meditation",
+      "Boost productivity through meditation",
+      "Achieve goals through meditation",
+    ],
+  },
+  {
+    title: "Diet And Nutrition Coach Certification: Beginner To Advanced",
+    premium: true,
+    bestseller: false,
+    updated: "June 2025",
+    duration: "5 total hours",
+    level: "All Levels",
+    subtitles: true,
+    description:
+      "Become A Nutrition Coach: Create Muscle Growth & Fat Loss Meal Plans, Build A Fitness Diet Business & Be A Health Expert",
+    points: [
+      "Earn your CPD Certificate",
+      "Design weight loss & muscle growth diets",
+    ],
+  },
+];
